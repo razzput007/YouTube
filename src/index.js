e5f1a5af5971a733c81687973d295d93965a28b0
razzput007 <dhoom.rk74@gmail.com>
2024-02-01 18:01:06 +0530

    Adding Utils

--- src/index.js
+++ src/index.js
@@ -11,4 +11,15 @@ dotenv.config({
     path:'./.env',
 });
 
-connectDB();
+connectDB().then(()=>{
+    app.on("error",(err)=>{
+        console.log(`Error occured ${err}`);
+        throw err;
+    })
+    app.listen(process.env.PORT || 7000,()=>{
+        console.log(`Server running on port ${process.env.PORT||"7000"}`);
+    })
+}).
+catch((error)=>{
+    console.log(error);
+});
